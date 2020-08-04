b43cdf3e6ac8ef97ce622d7197d0528d95f4a4d7
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2020-08-04 16:34:53 +0200

    WIP Tue Aug  4 16:34:53 CEST 2020

---
+++ src/content-scripts/o2_mail.ts
@@ -0,0 +1,2 @@
+(document.querySelector("[data-sel-elem=select_all_unread_objects]") as HTMLElement)?.click();
+(document.querySelector("[data-sel-elem=listing_delete_button]") as HTMLElement)?.click();
